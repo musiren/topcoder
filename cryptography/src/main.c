0e08398b96156ef650391508b4a72cc377179035
Jae-young Hwang <musiren@gmail.com>
2014-08-26 22:23:56 +0900

    Solved cryptography problem
    Local test done.
    
    Change-Id: I70a06abf36561a17dcf7f6489e7d295f73fce05a
    Signed-off-by: Jae-young Hwang <musiren@gmail.com>

--- cryptography/src/main.c
+++ cryptography/src/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long encrypt(long *numbers);
+#define PLUS_ONE    1
+
+long encrypt1(long *numbers);
+long encrypt2(long *numbers);
 int length;
 
 int main(int argc, char *argv[])
 {
     long *numbers;
-    long result;
     int i;
 
     if(argc > 1) {
@@ -18,20 +20,62 @@ int main(int argc, char *argv[])
     for(i = 0; i < length; i++) {
          *(numbers+i) = atoi(argv[i+1]);
     }
-    printf("%ld\n", encrypt(numbers));
+
+    printf("%ld\n", encrypt2(numbers));
 
     return 0;
 }
 
-long encrypt(long *numbers)
+long encrypt1(long *numbers)
 {
-    int i;
-    long max, tmp; 
+    int i, j;
+    long max = 1, tmp = 1;
 
     for(i = 0; i < length; i++) {
-        printf("%d ", *(numbers+i));
+        printf("%ld ", *(numbers+i));
     }
     printf("\n");
 
-    return -1;
+
+    for(i = 0; i < length; i++) {
+        *(numbers+i) += PLUS_ONE;
+//        printf("*numbers+%d) += PLUS ONE\n", i);
+        tmp = *numbers;
+//        printf("tmp = %ld\n", tmp);
+
+        for(j = 1; j < length; j++) {
+//            printf("i = %d, j = %d\n", i, j);
+            tmp = tmp * *(numbers+j);
+//            printf("tmp = %ld, *(number+%d) = %ld\n", tmp, j, *(numbers+j));
+        }
+        *(numbers+i) -= PLUS_ONE;
+
+        max = max >= tmp ? max : tmp;
+    }
+
+    return max;
+}
+
+
+long encrypt2(long *numbers)
+{
+    int i, j = 0;
+    long min = *numbers, max = 1;
+
+    for(i = 1; i < length; i++) {
+        if(min >= *(numbers+i)) {
+            min = *(numbers+i);
+            j = i;
+        }
+    }
+
+    printf("min = %ld\n", min);
+
+    *(numbers+j) += PLUS_ONE;
+    max = *numbers; 
+    for (i = 1; i < length; i++) {
+        max = max * *(numbers+i);
+    }
+     
+    return max;
 }
