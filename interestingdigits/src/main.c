301d7077344b6abaa88507c57742501472916def
Jae-young Hwang <musiren@gmail.com>
2015-05-10 16:33:05 +0900

    Add number system algorithm into interestingdigits
    Add numver system algorithm for probem
    
    Change-Id: Ief54ff658d915111b4b3c0c4359e9b543e707ece
    Signed-off-by: Jae-young Hwang <musiren@gmail.com>

--- interestingdigits/src/main.c
+++ interestingdigits/src/main.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "log.h"
-
+#define NUMBER 9999
+#define MAX_LEN 15
 int main(int argc, char *argv[])
 {
-	int base;
+	int base = 0;
+	int num = NUMBER;
+	char num_string[MAX_LEN];
+	int i = MAX_LEN-2;
+
+	memset(num_string, 32, sizeof(num_string));
 
     if (argc != 2) {
-        log_error("Usage: interesing-digits [BASE]");
+        printf("Usage: interesing-digits [BASE]\n");
         return -1;
-    } else if (0) {
+    } else {
+		base = atoi(argv[1]);
 	}
+
+	while (num >= base) {
+		int tmp = num % base;
+		if (tmp < 10) {
+			num_string[i] = (char)(tmp + 48);
+		} else {
+			num_string[i] = (char)(tmp + 55);
+		}
+		num = num / base;
+		i--;
+	}
+	num_string[i] = (char)((num % base) + 48);
+	num_string[MAX_LEN-1] = '\0';
+
+	printf("%s\n", num_string);
+
     return 0;
 }
