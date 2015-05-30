246cebe0820e0277e812eced9d5043e205cf9617
Jae-young Hwang <musiren@gmail.com>
2015-05-30 19:09:22 +0900

    bugfix
    Change-Id: I2acdbdcaa07ffb90d398af781f8ab3eab0638307
    Signed-off-by: Jae-young Hwang <musiren@gmail.com>

--- stock/src/main.c
+++ stock/src/main.c
@@ -4,15 +4,42 @@
 #define FALSE 0
 
 int n, N, K;
-int arr[10000];
+int arr[10001];
 int answer;
+int ret;
+
+int find(int current_pos, int count, int current_val) {
+	int i;
+
+	if (ret == K)
+		answer = TRUE;
+
+	for (i = current_pos+1; i < N; i++) {
+		if (current_val < arr[i]) {
+//			printf("find(arr[%d]=(%d), %d, arr[%d]=(%d)\n",
+//				i, arr[i], count+1, i, arr[i]);
+			if (find(i, count+1, arr[i])) {
+				ret = count+1;
+				if (ret == K)
+					answer = TRUE;
+				return TRUE;
+			}
+		} else {
+//			printf("find(arr[%d]=(%d), %d, %d\n",
+//				i, arr[i], count, current_val);
+			if(find(i, count, current_val)) {
+				ret = count;
+				return FALSE;
+			}
+		}
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	int i, j, k;
+	int i, j, k, l;
 
 	scanf("%d", &n);
-
 	for (i = 1; i <= n; i++) {
 		scanf("%d %d", &N, &K);
 		for (j = 0; j < N; j++) {
@@ -20,18 +47,15 @@ int main(int argc, char *argv[])
 		}
 
 		answer = FALSE;
-
 		for (j = 0; j < N; j++) {
-			int count = 1;
-			for (k = j + 1; k < N; k++) {
-				if(arr[j] < arr[k]) {
-					count++;
-				}
-			}
-			if (count > K)
-				answer = TRUE;
-		}
+			int count;
+//			printf("\n==============\nfind(arr[%d]=(%d), %d, arr[%d]=(%d)\n",
+//					j, arr[j], 1, j, arr[j]);
 
+			find(j, 1, arr[j]);
+			if (answer == TRUE)
+				break;
+		}
 		printf("Case #%d\n%d\n", i, answer);
 	}
 
